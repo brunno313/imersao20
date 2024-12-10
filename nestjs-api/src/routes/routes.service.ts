c0b3379a56497c91af04361e9f06b3b0f05c6425
Bruno Santana <brunoalexandre313@gmail.com>
2024-12-10 20:25:58 -0300

    feat: Implements patch and delete operations

--- nestjs-api/src/routes/routes.service.ts
+++ nestjs-api/src/routes/routes.service.ts
@@ -12,42 +12,14 @@ export class RoutesService {
   ) {}
 
   async create(createRouteDto: CreateRouteDto) {
-    console.log(createRouteDto);
-    const { available_travel_modes, geocoded_waypoints, routes, request } =
-      await this.directionsService.getDirections(
-        createRouteDto.source_id,
-        createRouteDto.destination_id,
-      );
+    const routeData = await this.getRouteData(
+      createRouteDto.name,
+      createRouteDto.source_id,
+      createRouteDto.destination_id,
+    );
 
-    const legs = routes[0].legs[0];
     return this.prismaService.route.create({
-      data: {
-        name: createRouteDto.name,
-        source: {
-          name: legs.start_address,
-          location: {
-            lat: legs.start_location.lat,
-            lng: legs.start_location.lng,
-          },
-        },
-        destination: {
-          name: legs.end_address,
-          location: {
-            lat: legs.end_location.lat,
-            lng: legs.end_location.lng,
-          },
-        },
-        duration: legs.duration.value,
-        distance: legs.distance.value,
-        directions: JSON.parse(
-          JSON.stringify({
-            available_travel_modes,
-            geocoded_waypoints,
-            routes,
-            request,
-          }),
-        ),
-      },
+      data: routeData,
     });
   }
 
@@ -61,11 +33,56 @@ export class RoutesService {
     });
   }
 
-  update(id: number, updateRouteDto: UpdateRouteDto) {
-    return `This action updates a #${id} route`;
+  async update(id: string, updateRouteDto: UpdateRouteDto) {
+    const routeData = await this.getRouteData(
+      updateRouteDto.name,
+      updateRouteDto.source_id,
+      updateRouteDto.destination_id,
+    );
+
+    return this.prismaService.route.update({
+      where: { id },
+      data: routeData,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} route`;
+  remove(id: string) {
+    return this.prismaService.route.delete({
+      where: { id },
+    });
+  }
+
+  async getRouteData(name: string, source: string, destination: string) {
+    const { available_travel_modes, geocoded_waypoints, routes, request } =
+      await this.directionsService.getDirections(source, destination);
+
+    const legs = routes[0].legs[0];
+    return {
+      name: name,
+      source: {
+        name: legs.start_address,
+        location: {
+          lat: legs.start_location.lat,
+          lng: legs.start_location.lng,
+        },
+      },
+      destination: {
+        name: legs.end_address,
+        location: {
+          lat: legs.end_location.lat,
+          lng: legs.end_location.lng,
+        },
+      },
+      duration: legs.duration.value,
+      distance: legs.distance.value,
+      directions: JSON.parse(
+        JSON.stringify({
+          available_travel_modes,
+          geocoded_waypoints,
+          routes,
+          request,
+        }),
+      ),
+    };
   }
 }
